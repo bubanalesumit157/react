a0eb04d2f0d3dbbc4d1f3d0e24651adfd8eb2c0d
bubanalesumit157 <bubanalesumit157@gmail.com>
2024-12-11 00:00:09 +0530

    commit

---
+++ 12MegaBlog/src/components/index.js
@@ -0,0 +1,12 @@
+import Header from "./header/header";
+import Footer from "./footer/footer";
+import Container from "./container/container"
+import Logo from "./Logo";
+import LogoutBtn from "./header/LogoutBtn";
+export default{
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
+}
